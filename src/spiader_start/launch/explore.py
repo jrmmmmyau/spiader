493e0ed1e04c3ed78dd5ad8e68f6c7f10ebff4bf
JRM-Dell <everyoneyau@gmail.com>
2026-07-16 21:39:38 -0400

    finished explore, havn't tested

---
+++ src/spiader_start/launch/explore.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    return LaunchDescription([
+		IncludeLaunchDescription(
+				PythonLaunchDescriptionSource([
+				os.path.join(get_package_share_directory('spiader_start'), 'launch', 'teleop_slam_cam.py')
+				])
+		),
+		IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
+            ]),
+            launch_arguments={
+                'params_file': os.path.join(
+                    get_package_share_directory('navigation'), 'yaml', 'nav2_params.yaml'
+                )
+            }.items()
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(get_package_share_directory('explore_lite'), 'launch', 'explore.launch.py')
+            ]),
+            launch_arguments={
+                'params_file': os.path.join(
+                    get_package_share_directory('navigation'), 'yaml', 'explore.yaml'
+                )
+            }.items()
+        )
+
+
+])
